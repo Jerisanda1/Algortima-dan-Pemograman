75c2657a481d31f0b5309612a2f44eaffa4480d0
Jerisanda1 <jerisandammmm@gmail.com>
2024-10-15 11:15:48 +0800

    Add files via upload

---
+++ Pertemuan 4/latihan9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ int main () {
+     int n;
+     cout << "enter a number : " ;
+     cin >> n ;
+
+     while (n > 0) {
+        cout << n << " ";
+        n--;
+cout << "lifttoff " <<endl;
+     }
+// memasukkan littoff kedalam kurung kurawal
+     return 0;
+ }
