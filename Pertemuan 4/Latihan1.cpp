dcc2acdcf8d7dc2ecd5fe65d4f6ad544b9caa631
Jerisanda1 <jerisandammmm@gmail.com>
2024-10-15 11:21:38 +0800

    Update Latihan1.cpp

--- Pertemuan 4/Latihan1.cpp
+++ Pertemuan 4/Latihan1.cpp
@@ -7,11 +7,12 @@ cout << "Enter first numbers: : ";
 cin >> a;
 cout << "Enter second numbers: :";
 cin>> b;
-
+// mengubah a dan b menjadi dua agar lebih rapi 
 if (a > b)
     cout <<"a is greather than b " << endl;
 else
     cout <<" b is greater than a" << endl ;
+    //menambahkan tanda ; pada akhir endl
 
     return 0;
 }
